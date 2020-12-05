41af94c9f7ea58137524d0f88edac1223e750700
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-12-06 01:56:34 +0300

    IOC Added

---
+++ BlogApp.Business/IOC/Microsoft/MicrosoftDependencies.cs
@@ -0,0 +1,30 @@
+﻿using BlogApp.Business.Abstract;
+using BlogApp.Business.Concrete;
+using BlogApp.DataAccess.Abstract;
+using BlogApp.DataAccess.Concrete.EntityFramework.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlogApp.Business.IOC.Microsoft
+{
+    public static class MicrosoftDependencies
+    {
+        public static void AddExtentions(this IServiceCollection services)
+        {
+            services.AddScoped<IAboutService, AboutManager>();
+            services.AddScoped<IAppUserService, AppUserManager>();
+            services.AddScoped<IContactService, ContactManager>();
+            services.AddScoped<IBlogService, BlogManager>();
+
+            services.AddScoped<IAboutRepository, AboutRepository>();
+            services.AddScoped<IAppUserRepository, AppUserRepository>();
+            services.AddScoped<IContactService, ContactManager>();
+            services.AddScoped<IBlogRepository, BlogRepository>();
+
+
+
+        }
+    }
+}
