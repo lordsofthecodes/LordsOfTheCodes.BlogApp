870fa9001e7ff094b62eb02210e6356a2aa5bf92
hasanfurkanfidan <62479737+hasanfurkanfidan@users.noreply.github.com>
2020-12-06 00:40:20 +0300

    Added genericRepositories

---
+++ BlogApp.DataAccess/Abstract/IGenericRepository.cs
@@ -0,0 +1,17 @@
+﻿using BlogApp.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BlogApp.DataAccess.Abstract
+{
+    public interface IGenericRepository<TEntity>where TEntity:class,IEntity,new()
+    {
+        List<TEntity> GetList(Expression<Func<TEntity, bool>> filter);
+        TEntity Get(Expression<Func<TEntity, bool>> filter);
+        void Add(TEntity entity);
+        void Delete(TEntity entity);
+        void Update(TEntity entity);
+    }
+}
---
+++ BlogApp.DataAccess/Concrete/EntityFramework/Repositories/GenericRepository.cs
@@ -0,0 +1,58 @@
+﻿using BlogApp.DataAccess.Abstract;
+using BlogApp.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace BlogApp.DataAccess.Concrete.EntityFramework.Repositories
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity:class,IEntity,new
+    {
+        Context context = new Context();
+
+        public void Add(TEntity entity)
+        {
+            using (context)
+            {
+                context.Add(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public void Delete(TEntity entity)
+        {
+            using (context)
+            {
+                context.Remove(entity);
+                context.SaveChanges();
+            }
+        }
+
+        public TEntity Get(Expression<Func<TEntity, bool>> filter)
+        {
+            using (context)
+            {
+                return context.Set<TEntity>().Where(filter).FirstOrDefault();
+            }
+        }
+
+        public List<TEntity> GetList(Expression<Func<TEntity, bool>> filter)
+        {
+            using (context)
+            {
+                return context.Set<TEntity>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(TEntity entity)
+        {
+            using (context)
+            {
+                context.Update(entity);
+                context.SaveChanges();
+            }
+        }
+    }
+}
